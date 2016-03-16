8d2104d7a98410980ac00c188cf8b1fbe6d53156
emsvasu <emsvasu@outlook.com>
2016-03-16 15:04:42 +0530

    Checking to trigger autmatic build on 3:04 PM

--- Githubtest/Githubtest/Program.cs
+++ Githubtest/Githubtest/Program.cs
@@ -10,7 +10,7 @@ namespace Githubtest
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hi Github. Triggered automatic build");
+            Console.WriteLine("Hi Github. Checking to trigger autmatic build on 3:04 PM");
         }
     }
 }
